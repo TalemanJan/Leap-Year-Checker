8bde36cde395e234ac0cd79d0884a000ae920d01
talemanjan <talemanaptech@gmail.com>
2023-03-16 04:19:22 +0500

    added js

--- app.js
+++ app.js
@@ -3,4 +3,15 @@
 function LeapOrNot(){
     var year = document.getElementById("year").value;
     
+if (year <= 1581){
+    alert("Year must be greater than 1581.");
+     }	
+    
+else if ((year % 4 == 0) && (year % 100 != 0)){
+    document.getElementById("result").textContent = " a leap!";
+    }
+else {
+        document.getElementById("result").textContent=("its not a leap year")
+    }
 }
+
