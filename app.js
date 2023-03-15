c6f280ed6e3f337cfd41fc92829db25923f8a088
talemanjan <talemanaptech@gmail.com>
2023-03-16 04:27:17 +0500

    added js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ if (year <= 1581){
      }	
     
 else if ((year % 4 == 0) && (year % 100 != 0)){
-    document.getElementById("result").textContent = " a leap!";
+    document.getElementById("result").textContent = "It's a leap!";
     }
 else {
         document.getElementById("result").textContent=("its not a leap year")
